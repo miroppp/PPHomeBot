16202414788e044b8e768b64daf1969d606cbbab
Miro Lu <miroppp@gmail.com>
2018-02-08 16:19:17 +0800

    for heroku server changes

--- index.js
+++ index.js
@@ -5,10 +5,14 @@ var express = require('express')
 var app = express();
 app.use(bodyParser.json());
 
+app.get('/', function (req, res) {
+    res.send("Hello");
+  });
+  
 app.post('/', function (req, res) {
   console.log(JSON.stringify(req.body));
 });
 
-app.listen(3000, function () {
+app.listen(process.evn.PORT || 3000, function () {
   console.log('Example app listening on port 3000!');
 });

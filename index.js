c8025b6b2e4c1be31226f810b01929cbdc62c124
Miro Lu <miroppp@gmail.com>
2018-02-08 15:37:13 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+import { read } from 'fs';
+
+var express = require('express')
+    , bodyParser = require('body-parser'
+    , request = require('request') ) ;
+
+var app = express();
+app.use(bodyParser.json());
+
+app.post('/', function (req, res) {
+  console.log(JSON.stringify(req.body));
+});
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});
